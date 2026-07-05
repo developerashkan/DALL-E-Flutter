7d2bd5d9a3449f33f6eea6cd76b649e4f81514a8
Ashkan Bahmani <74037732+developerashkan@users.noreply.github.com>
2026-07-05 13:20:34 +0400

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,30 +1,41 @@
 import 'dart:async';
-import 'package:dall_e/dalle_generate_screen.dart';
 import 'package:flutter/material.dart';
+import 'dalle_generate_screen.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({super.key});
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
   void initState() {
-    Timer(
-        const Duration(seconds: 2),
-            () => Navigator.of(context).pushReplacement(MaterialPageRoute(
-            builder: (BuildContext context) => const DallEGenerateScreen())));
-
     super.initState();
+    _navigateToHome();
+  }
+
+  Future<void> _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 2));
+    
+    if (!mounted) return; 
+
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const DallEGenerateScreen()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Image.asset("assets/logo-no-background.png",width: 250,height: 250,),),
+      body: Center(
+        child: Image.asset(
+          "assets/logo-no-background.png",
+          width: 250,
+          height: 250,
+        ),
+      ),
     );
   }
 }
